3f05adb71ccf050d038e2fe49497760e60f84ce5
Aswin N A <aswinanand314@gmail.com>
2026-02-28 20:01:52 +0530

    all completed

--- frontend/src/page/PageDemo.jsx
+++ frontend/src/page/PageDemo.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./PageDemo.css";
 const PageDemo = () => {
   return (
-    <div>
-      <header>Welcome To Task Management Application</header>
-      <h2>Explore the features of our Task Management Application </h2>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+    <div className="home-container">
+      <div className="home-card">
+        <h1 className="home-title">Task Management Application</h1>
+
+        <p className="home-description">
+          Organize your tasks, boost productivity, and manage your workflow
+          efficiently.
+        </p>
+
+        <div className="home-buttons">
+          <Link to="/login">
+            <button className="login-btn">Login</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
